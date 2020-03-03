0554a71a6b3d0b800c6070475ccc49eb2d60a2aa
ror32 <ror32@users.noreply.github.com>
2020-03-03 17:02:32 -0500

    initial commit

--- builtinla/settings.py
+++ builtinla/settings.py
@@ -18,7 +18,7 @@ NEWSPIDER_MODULE = 'builtinla.spiders'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'builtinla (+http://www.yourdomain.com)'
 
-# Obey robots.txt rules
+# Obey robots.txt rules 
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
